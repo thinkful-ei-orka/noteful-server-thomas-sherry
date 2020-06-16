ffda7f0491ba696a7e4c4265fa5857d47bfb03d0
Sherry McCarty <sherrydmccarty@gmail.com>
2020-06-16 11:51:31 -0400

    GET issues

---
+++ src/folders/folders-service.js
@@ -0,0 +1,8 @@
+const FoldersService = {
+  getAllFolders(knex) {
+    return knex.select('*').from('folders')
+  },
+  
+}
+
+module.exports = FoldersService
--- test/folders.spec.js
+++ test/folders.spec.js
@@ -1,9 +1,51 @@
 const app = require('../src/app')
+const FoldersService = require('../src/folders/folders-service')
+const { expect } = require('chai')
+const { TEST_DB_URL } = require('../src/config')
+const knex = require('knex');
 
 describe('Folders Endpoints', () => {
-  it('GET / responds with 200 containing an empty array', () => {
+  let db;
+
+  before(() => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    })
+    app.set('db', db)
+  })
+ 
+  before(() => db('folders').truncate());
+  afterEach(() => db('folders').truncate());
+  after(() => db.destroy());
+ 
+  it('GET /folders responds with 200 containing an empty array', () => {
     return supertest(app)
       .get('/folders')
       .expect(200, [])
+  })
+
+  context('Given noteful has data', () => {
+    const testFolders = [
+      {
+        "id": "1",
+        "name": "Important"
+      },
+      {
+        "id": "2",
+        "name": "Super"
+      },
+      {
+        "id": "3",
+        "name": "Spangley"
+      }
+    ]
+
+    it('GET should respond with 200 and all the folders', () => {
+      return FoldersService.getAllFolders(db)
+        .then(actual => {
+          expect(actual).to.eql(testFolders);
+        })
+    })
   })
 })
